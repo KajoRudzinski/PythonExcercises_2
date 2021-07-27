9300eb85911ec65e00e7c055ada047e044b381ce
LAPTOP-JPL0M3U7\kajor <kajo.rudzinski@gmail.com>
2021-07-27 23:21:16 +0200

    reading titanic csv with numpy

--- read_txt_basic.py
+++ read_txt_basic.py
@@ -12,3 +12,23 @@ file.close()
 
 # Check whether file is closed
 print(file.closed)
+
+# #############################
+# Read & print the first 3 lines
+with open('sth_bigger.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+
+# #######################
+# Read csv with Numpy
+import numpy as np
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file, encoding=None)
+
+# Print out first two entries of d
+print(d[:2])
