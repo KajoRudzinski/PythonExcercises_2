9889958e1b8209e965704dfe34e8fa7f854583b4
LAPTOP-JPL0M3U7\kajor <kajo.rudzinski@gmail.com>
2021-07-27 22:11:18 +0200

    basic txt read

---
+++ read_txt_basic.py
@@ -0,0 +1,14 @@
+# Open a file: file
+file = open('something.txt', mode='r')
+
+# Print it
+print(file.read())
+
+# Check whether file is closed
+print(file.closed)
+
+# Close file
+file.close()
+
+# Check whether file is closed
+print(file.closed)
